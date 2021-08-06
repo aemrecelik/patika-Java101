a57aa1e38af7567edc9af9171bb346b8c4133188
aemrecelik <ecelik576@gmail.com>
2021-08-06 12:46:39 +0300

    Pratik 11 Java 101

--- README.md
+++ README.md
@@ -107,6 +107,40 @@ Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi 
 **Ödev**
 
 Girilen 3 sayıyı "küçükten büyüğe" sıralayan programı yazınız.
+
+- [Pratik11 - Burç Bulan Program](src/Pratikler/Pratik11.java)
+
+Java koşullu ifadeler ile kullanıcının burcunu bulan program yapımı.
+
+Koç Burcu : 21 Mart - 20 Nisan
+
+Boğa Burcu : 21 Nisan - 21 Mayıs
+
+İkizler Burcu : 22 Mayıs - 22 Haziran
+
+Yengeç Burcu : 23 Haziran - 22 Temmuz
+
+Aslan Burcu : 23 Temmuz - 22 Ağustos
+
+Başak Burcu : 23 Ağustos - 22 Eylül
+
+Terazi Burcu : 23 Eylül - 22 Ekim
+
+Akrep Burcu : 23 Ekim - 21 Kasım
+
+Yay Burcu : 22 Kasım - 21 Aralık
+
+Oğlak Burcu : 22 Aralık - 21 Ocak
+
+Kova Burcu : 22 Ocak - 19 Şubat
+
+Balık Burcu : 20 Şubat - 20 Mart
+
+**Ödev**
+
+Aynı örneği switch-case kullanmadan yapınız.
+
+
 ### Ödevler
 - [Ödev1 - Vücut Kitle Endeksi Hesaplayan Program](src/Odevler/Odev1.java)
 
--- src/Pratikler/Prartik11.java
+++ src/Pratikler/Prartik11.java
@@ -1,4 +1,104 @@
 package Pratikler;
 
+import java.util.Scanner;
+
 public class Prartik11 {
+
+    public static void main(String[] args) {
+        int gun, ay;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hangi ayda doğdunuz: \n1-Ocak\n2-Şubat\n3-Mart\n4-Nisan\n5-Mayıs\n6-Haziran\n7-Temmuz\n8-Ağustos\n9-Eylül\n10-Ekim\n11-Kasım\n12-Aralık");
+        ay = scanner.nextInt();
+
+        System.out.println("Ayın Kaçıncı Günü Doğdunuz");
+        gun = scanner.nextInt();
+
+        if (ay == 1) {
+
+            if (gun <= 21) {
+                System.out.println("Oğlak Burcusunuz.");
+            } else
+                System.out.println("Kova Burcusunuz");
+
+        } else if (ay == 2) {
+
+            if (gun <= 19) {
+                System.out.println("Kova Burcusunuz.");
+            } else
+                System.out.println("Balık Burcusunuz");
+
+        } else if (ay == 3) {
+
+            if (gun <= 20) {
+                System.out.println("Balık Burcusunuz.");
+            } else
+                System.out.println("Koç Burcusunuz");
+
+        } else if (ay == 4) {
+
+            if (gun <= 20) {
+                System.out.println("Koç Burcusunuz.");
+            } else
+                System.out.println("Boğa Burcusunuz");
+
+        } else if (ay == 5) {
+
+            if (gun <= 21) {
+                System.out.println("Boğa Burcusunuz.");
+            } else
+                System.out.println("İkizler Burcusunuz");
+
+        } else if (ay == 6) {
+
+            if (gun <= 22) {
+                System.out.println("İkizler Burcusunuz.");
+            } else
+                System.out.println("Yengeç Burcusunuz");
+
+        } else if (ay == 7) {
+
+            if (gun <= 22) {
+                System.out.println("Yengeç Burcusunuz.");
+            } else
+                System.out.println("Aslan Burcusunuz");
+
+        } else if (ay == 8) {
+
+            if (gun <= 22) {
+                System.out.println("Aslan Burcusunuz.");
+            } else
+                System.out.println("Başak Burcusunuz");
+
+        } else if (ay == 9) {
+
+            if (gun <= 22) {
+                System.out.println("Başak Burcusunuz.");
+            } else
+                System.out.println("Terazi Burcusunuz");
+
+        } else if (ay == 10) {
+
+            if (gun <= 22) {
+                System.out.println("Terazi Burcusunuz.");
+            } else
+                System.out.println("Akrep Burcusunuz");
+
+        } else if (ay == 11) {
+
+            if (gun <= 21) {
+                System.out.println("Akrep Burcusunuz.");
+            } else
+                System.out.println("Yay Burcusunuz");
+
+        } else if (ay == 12) {
+
+            if (gun <= 21) {
+                System.out.println("Yay Burcusunuz.");
+            } else
+                System.out.println("Oğlak Burcusunuz");
+
+        } else
+            System.out.println("Geçerli bir ay girmediniz.");
+    }
 }
