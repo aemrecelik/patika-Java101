a4ceeaaf70d51d40062c40460ef0763a90dc7839
aemrecelik <ecelik576@gmail.com>
2021-08-07 21:14:51 +0300

    Ödev 11 Java 101

--- README.md
+++ README.md
@@ -368,3 +368,7 @@ Fibonacci serisi, her sayının kendinden önceki ile toplanması sonucu oluşan
 Fibonacci dizisi, 0'dan başlar ve sonsuza kadar. Her rakam, bir önceki rakamla toplanır. Elde edilen sonuç rakamın sağ tarafına yazılır. Fibonacci dizisinin ilk on sayısı şu şekildedir:
 
 9 Elemanlı Fibonacci Serisi : 0 1 1 2 3 5 8 13 21 34
+
+- [Ödev11 - Üs Hesabı Yapan Program](src/Odevler/Odev11.java)
+
+Java dilinde, taban ve üs değerleri kullanıcıdan alınan üs alma işlemini "Recursive" metot kullanarak yapan programı yazınız.
---
+++ src/Odevler/Odev11.java
@@ -0,0 +1,25 @@
+package Odevler;
+
+import java.util.Scanner;
+
+public class Odev11 {
+    static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Üs değeri giriniz :");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+    public static void main(String[] args) {
+        while (true){
+            power();
+        }
+    }
+}
